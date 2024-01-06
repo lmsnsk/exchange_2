d842feb61933758f44b5d627004d510b942b9e3c
glengunt <lmslms87@yandex.ru>
2024-01-06 16:05:06 +0700

    11

--- tests/tests.c
+++ tests/tests.c
@@ -48,11 +48,13 @@ Suite *suite_create_matrix(void) {
   return s;
 }
 
-void run_testcase(Suite *testcase) {
-  static int counter_testcase = 1;
+double get_rand(double min, double max) {
+  double val = (double)rand() / RAND_MAX;
+  return min + val * (max - min);
+}
 
-  if (counter_testcase > 0) putchar('\n');
-  counter_testcase++;
+void run_testcase(Suite *testcase) {
+  printf("\n");
   SRunner *sr = srunner_create(testcase);
 
   srunner_set_fork_status(sr, CK_NOFORK);
@@ -61,23 +63,7 @@ void run_testcase(Suite *testcase) {
   srunner_free(sr);
 }
 
-double get_rand(double min, double max) {
-  double val = (double)rand() / RAND_MAX;
-  return min + val * (max - min);
-}
-
 int main(void) {
-  Suite *list_cases[] = {suite_create_matrix(),
-                         // suite_eq_matrix(),
-                         //  suite_sub_matrix(),       suite_sum_matrix(),
-                         //  suite_mult_matrix(), suite_mult_number_matrix(),
-                         //  suite_transpose_matrix(), suite_calc_complements(),
-                         //  suite_determinant(),      suite_inverse_matrix(),
-                         //  suite_matrix_gather(),
-                         NULL};
-  for (Suite **current_testcase = list_cases; *current_testcase != NULL;
-       current_testcase++) {
-    run_testcase(*current_testcase);
-  }
+  run_testcase(suite_create_matrix());
   return 0;
 }
